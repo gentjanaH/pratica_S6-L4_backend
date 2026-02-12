f48ab8a4644141cb9fc45ed4963b6ebe41b5a95e
Gentjana <hanigentjana@gmail.com>
2026-02-12 14:17:32 +0100

    add_pratica

---
+++ src/main/java/gentjanahani/u2w6d4/U2w6d4Application.java
@@ -0,0 +1,13 @@
+package gentjanahani.u2w6d4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2w6d4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2w6d4Application.class, args);
+	}
+
+}
---
+++ src/test/java/gentjanahani/u2w6d4/U2w6d4ApplicationTests.java
@@ -0,0 +1,13 @@
+package gentjanahani.u2w6d4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2w6d4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
